d8a1bf1dc8473f61afa9116f4f68b61487e5299a
Neveen97 <neveenaburomman1@gmail.com>
2022-05-06 04:01:57 +0300

    lab26

---
+++ resty-app/src/App.js
@@ -0,0 +1,22 @@
+ 
+ import './App.css';
+
+import Header from './components/header/header'
+import Form from './components/form/form'
+import Navbar from './components/NavBar/navbar';
+import Footer from './components/Footer/footer';
+
+
+function App() {
+  return (
+    <>
+
+    <Header/>
+    <Navbar />
+    <Form/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
